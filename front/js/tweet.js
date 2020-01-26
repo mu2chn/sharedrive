c929feed0aecc4b4f271bba89d9ec14cdedc5179
Hiroki <face9363>
2020-01-26 23:07:51 +0900

    connect to my server

--- front/js/tweet.js
+++ front/js/tweet.js
@@ -7,7 +7,7 @@ function tweet() {
                 const loading = document.querySelector('#loading');
                 loading.style.display = "block";
                 const url = await API.sendImg(e.target.files[0]);
-                const content = encodeURI(`科目名：\n教授名：\n画像リンク：${url}\n年度：2020 前期`);
+                const content = encodeURI(`科目名：\n教授名：\n画像リンク：https://engine.face9363.net/imgur?img=${encodeURI(url.split('/').pop())}\n年度：2020 前期`);
                 const href = `https://twitter.com/intent/tweet?screen_name=ku_search&text=${content}`;
                 atag.href = href;
                 setTimeout(()=>{
